f1c8a4476b549e3517dc817feec3c3f10ba5b8c4
SSC星瀚资本 <jass.ada@qq.com>
2016-03-18 18:42:46 +0800

    crawl something

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ REQ_AMOUNTS = 10
 #############- your config items -########################
 
 HEADERS = {
-    
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36"
+
 }
 
 
--- processData.py
+++ processData.py
@@ -1,14 +1,33 @@
 import asyncio
-import pyquery
+import pyquery as pq
+import config
 
 if config.MYSQL:
-    import aiomysqlS
+    import aiomysql
 
 @asyncio.coroutine
 def processData(data):
     '''
     data is from the http response in main module.
     '''
+    
+    pass
+
+@asyncio.coroutine
+def Xm(data):
+    d=pq.PyQuery(data)
+
+    print(d('p:first'))
+    pass
+
+async def Asto(data):
+    d=pq.PyQuery(data)
+    print(d(''))
+    pass 
+
+async def Calendar(data):
+    d=pq.PyQuery(data)
+    print(data[1000:1050])
     pass
 
 if __name__ == '__main__':
